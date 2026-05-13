eae4b9b76944a2773cf08f559723a61d9f5b5e10
vishvatejsurve <vishvatejsurve93@gmail.com>
2026-05-13 14:00:25 +0530

    Added Swagger documentation and API response annotations

--- src/main/java/com/servepay_platform/controller/AuthController.java
+++ src/main/java/com/servepay_platform/controller/AuthController.java
@@ -4,6 +4,10 @@ import com.servepay_platform.dto.securityDTO.AuthResponseDTO;
 import com.servepay_platform.dto.securityDTO.LoginRequestDTO;
 import com.servepay_platform.dto.securityDTO.RegisterRequestDTO;
 import com.servepay_platform.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,18 +17,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Authentication APIs",
+    description = "APIs for user login and registration")
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final AuthService authService;
 
+    @Operation(
+            summary = "Register new user",
+            description = "Registers customer or provider account"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",
+            description = "User registered successfully"),
+            @ApiResponse(responseCode = "400",
+            description = "Invalid request"),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "User already exists"
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequestDTO request){
         authService.registerUser(request);
         return ResponseEntity.ok("User registered successfully");
     }
 
+    @Operation(
+            summary = "Login user",
+            description = "Authenticate user and generate JWT token"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Login successful"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Invalid email or password"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Bad request"
+            )
+    })
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginRequestDTO request){
         return ResponseEntity.ok(authService.login(request));
--- src/main/java/com/servepay_platform/controller/ServiceController.java
+++ src/main/java/com/servepay_platform/controller/ServiceController.java
@@ -2,8 +2,11 @@ package com.servepay_platform.controller;
 
 import com.servepay_platform.dto.serviceDTO.ServiceRequestDTO;
 import com.servepay_platform.dto.serviceDTO.ServiceResponseDTO;
-import com.servepay_platform.entity.ServiceOffering;
 import com.servepay_platform.service.OfferingService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,24 +16,66 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Tag(name = "Service Management APIs",
+description = "APIs for addService,getMyService,getAllService")
 @RequestMapping("/services")
 @RequiredArgsConstructor
 public class ServiceController {
 
     private final OfferingService offeringService;
 
+    @Operation(
+            summary = "Create Service",
+            description = "Allows service providers to create and publish a new service listing"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Service added successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid service details"
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized access"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Provider not found"
+            )
+    })
     @PostMapping
     public ResponseEntity<String> addService(@RequestBody ServiceRequestDTO dto, Authentication authentication){
        String email = authentication.getName();
         offeringService.addService(dto,email);
         return new ResponseEntity<>("Service added Successfully",HttpStatus.CREATED);
     }
+    @Operation(
+            summary = "Fetch provider Service",
+            description = "Retrieves all services created by the authenticated provider"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Service fetched successfully"),
+            @ApiResponse(responseCode = "404", description = "No service found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/my")
     public ResponseEntity<List<ServiceResponseDTO>> getMyServices(Authentication authentication){
         String email = authentication.getName();
         List<ServiceResponseDTO> myServices = offeringService.getMyServices(email);
         return ResponseEntity.ok(myServices);
     }
+    @Operation(
+            summary = "Fetch All Services",
+            description = "Retrieves all available services with provider details"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Services fetched successfully"),
+            @ApiResponse(responseCode = "404", description = "No services found"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping
     public ResponseEntity<List<ServiceResponseDTO>> getAllService(){
         List<ServiceResponseDTO> allService = offeringService.getAllService();
